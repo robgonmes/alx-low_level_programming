9e2c34e979bf9a9ce4219a74eab2d7978ba4dbc6
Robert Mesleh <meslehrobert3@gmail.com>
2023-03-22 06:49:58 -0700

    Add project task 1 - 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two integers.
+ * @n1: First integer to add
+ * @n2: Second integer to add
+ *
+ * Return: The result of the addition.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
